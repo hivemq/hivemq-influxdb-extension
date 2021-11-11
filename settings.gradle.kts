e7429367f900fad4be5e780a3068671b81f2fc42
Silvio Giebl <silvio.giebl@hivemq.com>
2021-11-11 17:44:13 +0100

    build > migrate to gradle

---
+++ settings.gradle.kts
@@ -0,0 +1,10 @@
+rootProject.name = "hivemq-influxdb-extension"
+
+pluginManagement {
+    plugins {
+        id("com.hivemq.extension") version "${extra["plugin.hivemq-extension.version"]}"
+        id("com.github.hierynomus.license") version "${extra["plugin.license.version"]}"
+        id("com.github.sgtsilvio.gradle.utf8") version "${extra["plugin.utf8.version"]}"
+        id("org.asciidoctor.jvm.convert") version "${extra["plugin.asciidoctor.version"]}"
+    }
+}
