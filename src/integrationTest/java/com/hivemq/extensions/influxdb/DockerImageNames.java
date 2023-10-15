faadacd272c1c9557a8b84b7e360a1761440a829
Silvio Giebl <silvio.giebl@hivemq.com>
2023-10-15 17:55:54 +0200

    Update hivemq extension gradle plugin to 3.1.0 Replace utf8 with defaults plugin

--- src/integrationTest/java/com/hivemq/extensions/influxdb/DockerImageNames.java
+++ src/integrationTest/java/com/hivemq/extensions/influxdb/DockerImageNames.java
@@ -15,7 +15,7 @@
  */
 package com.hivemq.extensions.influxdb;
 
-import com.hivemq.extension.sdk.api.annotations.NotNull;
+import org.jetbrains.annotations.NotNull;
 import org.testcontainers.utility.DockerImageName;
 
 /**
