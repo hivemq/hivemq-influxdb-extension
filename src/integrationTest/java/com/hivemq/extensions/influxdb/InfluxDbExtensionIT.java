a94bd585abe6966ee82ba13566832315c92e8ebf
David Sondermann <david.sondermann@hivemq.com>
2025-11-18 18:24:48 +0100

    Update testcontainers to 2.0.2

--- src/integrationTest/java/com/hivemq/extensions/influxdb/InfluxDbExtensionIT.java
+++ src/integrationTest/java/com/hivemq/extensions/influxdb/InfluxDbExtensionIT.java
@@ -16,13 +16,13 @@
 package com.hivemq.extensions.influxdb;
 
 import com.hivemq.client.mqtt.mqtt5.Mqtt5Client;
+import com.influxdb.client.InfluxDBClient;
+import com.influxdb.client.InfluxDBClientFactory;
+import com.influxdb.client.domain.InfluxQLQuery;
 import io.github.sgtsilvio.gradle.oci.junit.jupiter.OciImages;
-import org.influxdb.InfluxDB;
-import org.influxdb.dto.Query;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.InfluxDBContainer;
 import org.testcontainers.containers.Network;
 import org.testcontainers.hivemq.HiveMQContainer;
@@ -31,15 +31,12 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.MountableFile;
 
 import static org.awaitility.Awaitility.await;
-import static org.influxdb.querybuilder.BuiltQuery.QueryBuilder.select;
 
 @SuppressWarnings({"resource"})
 @Testcontainers
 public class InfluxDbExtensionIT {
 
-    private static final @NotNull String INFLUXDB_NAME = "hivemq";
-
-    private static final @NotNull Logger LOG = LoggerFactory.getLogger(InfluxDbExtensionIT.class);
+    private static final @NotNull String INFLUXDB_DATABASE = "hivemq";
 
     private final @NotNull Network network = Network.newNetwork();
 
@@ -50,7 +47,8 @@ public class InfluxDbExtensionIT {
                     .withNetwork(network)
                     .withCopyToContainer(MountableFile.forClasspathResource("influxdb.properties"),
                             "/opt/hivemq/extensions/hivemq-influxdb-extension/influxdb.properties")
-                    .withLogConsumer(outputFrame -> System.out.print("HIVEMQ: " + outputFrame.getUtf8String()));
+                    .withLogConsumer(outputFrame -> System.out.print("HIVEMQ: " + outputFrame.getUtf8String()))
+                    .withEnv("HIVEMQ_DISABLE_STATISTICS", "true");
 
     @Container
     private final @NotNull InfluxDBContainer<?> influxDB =
@@ -60,49 +58,51 @@ public class InfluxDbExtensionIT {
 
     @Test
     void testMetricsAreForwardedToInfluxDB() {
-        final var influxDbClient = influxDB.getNewInfluxDB();
-        influxDbClient.setDatabase("hivemq");
+        try (final var influxDBClient = InfluxDBClientFactory.create(influxDB.getUrl())) {
+            final var createDbQuery = new InfluxQLQuery("CREATE DATABASE \"%s\"".formatted(INFLUXDB_DATABASE), "");
+            influxDBClient.getInfluxQLQueryApi().query(createDbQuery);
 
-        final var query = influxDbClient.query(new Query("CREATE DATABASE \"" + INFLUXDB_NAME + "\""));
-        LOG.info("created database with query result: {}", query);
-        influxDbClient.setDatabase(INFLUXDB_NAME);
+            final var mqttClient =
+                    Mqtt5Client.builder().serverHost(hivemq.getHost()).serverPort(hivemq.getMqttPort()).buildBlocking();
+            mqttClient.connect();
+            mqttClient.publishWith().topic("my/topic1").send();
+            mqttClient.publishWith().topic("my/topic2").send();
+            mqttClient.publishWith().topic("my/topic3").send();
+            mqttClient.disconnect();
 
-        final var mqttClient =
-                Mqtt5Client.builder().serverHost(hivemq.getHost()).serverPort(hivemq.getMqttPort()).buildBlocking();
-        mqttClient.connect();
-        mqttClient.publishWith().topic("my/topic1").send();
-        mqttClient.publishWith().topic("my/topic2").send();
-        mqttClient.publishWith().topic("my/topic3").send();
-        mqttClient.disconnect();
-
-        await().until(() -> getMetricMax(influxDbClient, "com.hivemq.messages.incoming.publish.count") == 3);
-        await().until(() -> getMetricMax(influxDbClient, "com.hivemq.messages.incoming.connect.count") == 1);
+            await().until(() -> getMetricMax(influxDBClient, "com.hivemq.messages.incoming.publish.count") == 3);
+            await().until(() -> getMetricMax(influxDBClient, "com.hivemq.messages.incoming.connect.count") == 1);
+        }
     }
 
-    private long getMetricMax(final @NotNull InfluxDB client, final @NotNull String metric) {
-        var acc = 0L;
-        final var queryResult = client.query(select("count").from(INFLUXDB_NAME, metric));
-        for (final var result : queryResult.getResults()) {
-            final var series = result.getSeries();
-            if (series == null) {
-                break;
-            }
-            final var values = series.get(series.size() - 1).getValues();
-            if (values == null) {
-                break;
-            }
-            long max = 0;
-            for (final var value : values) {
-                if (value == null) {
-                    break;
-                }
-                final var val = (double) value.get(1);
-                if (max < val) {
-                    max = (long) val;
+    private long getMetricMax(final @NotNull InfluxDBClient client, final @NotNull String metric) {
+        final var influxQL = String.format("SELECT MAX(count) FROM \"%s\"", metric);
+        final var query = new InfluxQLQuery(influxQL, INFLUXDB_DATABASE);
+        final var result = client.getInfluxQLQueryApi().query(query);
+        long max = 0;
+        for (final var queryResult : result.getResults()) {
+            for (final var series : queryResult.getSeries()) {
+                for (final var record : series.getValues()) {
+                    final var value = getValue(record.getValueByKey("max"));
+                    if (value > max) {
+                        max = value;
+                    }
                 }
             }
-            acc += max;
         }
-        return acc;
+        return max;
+    }
+
+    private static long getValue(final @Nullable Object valueField) {
+        if (valueField instanceof Number) {
+            return ((Number) valueField).longValue();
+        } else if (valueField != null) {
+            try {
+                // try to parse as double if it's a string
+                return (long) Double.parseDouble(valueField.toString());
+            } catch (final NumberFormatException ignored) {
+            }
+        }
+        return Long.MIN_VALUE;
     }
 }
