5f6c58b9cb4ad245947f8075e738b27be80aac64
David Sondermann <david.sondermann@hivemq.com>
2026-03-04 12:03:54 +0100

    Replace license plugin with Spotless 8.3.0

--- src/main/java/com/hivemq/extensions/influxdb/InfluxDbV3Sender.java
+++ src/main/java/com/hivemq/extensions/influxdb/InfluxDbV3Sender.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.hivemq.extensions.influxdb;
 
 import com.izettle.metrics.influxdb.InfluxDbHttpSender;
